d4e3a2521066cbe298539ed3f4f311bd8739622e
Sayeeduddin <iamsayeeduddin@outlook.com>
2023-03-02 11:57:37 +0530

    Error Handling

--- utils/appLogger.js
+++ utils/appLogger.js
@@ -2,10 +2,13 @@ const bunyan = require("bunyan");
 const path = require("path");
 const fs = require("fs");
 
-const dir = path.join(__dirname, "logs");
+const dir = path.join(__dirname, "..", "logs");
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
+  if (!fs.existsSync(path.join(dir, "app.log"))) {
+    fs.writeFile(path.join(dir, "app.log"));
+  }
 }
 const filePath = path.join(__dirname, "..", "logs", "app.log");
 
