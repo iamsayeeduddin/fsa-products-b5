55b3a5f8ebb33e65b9c77f755b7cfc223fb9456d
Sayeeduddin <iamsayeeduddin@outlook.com>
2023-03-02 11:50:29 +0530

    App Logger Error Handled

--- utils/appLogger.js
+++ utils/appLogger.js
@@ -1,6 +1,12 @@
 const bunyan = require("bunyan");
 const path = require("path");
+const fs = require("fs");
 
+const dir = path.join(__dirname, "logs");
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
 const filePath = path.join(__dirname, "..", "logs", "app.log");
 
 const logger = bunyan.createLogger({
